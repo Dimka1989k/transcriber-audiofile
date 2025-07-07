719eb3a7f2284c61002f345928a04142468b5435
Dmytro Kyselov <dima1989k@ukr.net>
2025-07-08 01:46:14 +0300

    add config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   experimental: {
+    // @ts-expect-error: outputFileTracingIgnores is not in type definitions yet
+    outputFileTracingIgnores: ['./lib/generated/prisma/**/*'],
+  },
 };
 
 export default nextConfig;
