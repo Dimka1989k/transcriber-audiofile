023c825b824641083eb6c5616d2aba8b24b22cf2
Dmytro Kyselov <dima1989k@ukr.net>
2025-07-08 11:37:05 +0300

    delete error

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,12 @@
-import 'server-only'
+import 'server-only';
+import Stripe from 'stripe';
 
-import Stripe from 'stripe'
+const secretKey = process.env.STRIPE_SECRET_KEY;
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+if (!secretKey) {
+  throw new Error("Missing STRIPE_SECRET_KEY in environment variables");
+}
+
+export const stripe = new Stripe(secretKey, {
+  apiVersion: '2025-06-30.basil', 
+});
